0db8e8cf01373aafe2681331f4b78e3a439a2de4
Vlad-Re <181209781+Vlad-Re@users.noreply.github.com>
2025-10-16 18:34:27 +0300

    also new

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use VerrryUniqueProject::list_parser;
+use redchuk_parser::list_parser;
 
 pub fn main() {
-    assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
+    assert_eq!(
+        list_parser::list("[1,1,2,3,5,8]"),
+        Ok(vec![1, 1, 2, 3, 5, 8])
+    );
 }
